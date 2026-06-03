a7adc58b5550d4cc3666da1b2621508b77e01437
nazard027-prog <nazard027@gmail.com>
2026-06-03 11:49:18 +0300

    +DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -103,4 +103,18 @@ class ShoppingList:
         result._items = self._items + other._items
         return result
 
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
 
+    def scale(self, ratio):
+        scaled_recipe = super().scale(ratio)
+        return DietaryRecipe(
+            self.title,
+            self.diet_type,
+            scaled_recipe.ingredients,
+        )
+
+    def __str__(self):
+        return f"[{self.diet_type}] {super().__str__()}"
